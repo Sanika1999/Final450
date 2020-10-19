ec2785af5e0c46d784a4040d3c55b37d52ef74d2
Sanika <51399803+Sanika1999@users.noreply.github.com>
2020-10-20 03:00:59 +0530

    Add files via upload

---
+++ Reverse_array/reverse_by_swapping.java
@@ -0,0 +1,37 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class reverse_by_swapping {
+	public static void reverse(int[] a) {
+		int temp;
+		int start=0;
+		int end=a.length-1;
+		for(int i=0;i<a.length/2;i++) {
+			temp=a[start];
+			a[start]=a[end];
+			a[end]=temp;
+			start=start+1;
+			end=end-1;
+			
+		}
+		for(int j=0;j<a.length;j++) {
+			System.out.println(a[j]);
+			
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter number of elements");
+		int n=sc.nextInt();
+		int[] arr= new int[n];
+		for(int k=0;k<n;k++) {
+			System.out.println("enter element");
+			int x=sc.nextInt();
+			arr[k]=x;
+		}
+		reverse(arr);
+	}
+
+}
